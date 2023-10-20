fe4b98cd148c5837319bed2c7e75c7ee515138fe
Abednego Emonena <emonenaabednego@gmail.com>
2023-10-20 16:38:45 +0100

    created a custom user manager

---
+++ chacha/managers.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import UserManager
+from django.db import models
+
+class CustomUserManager(UserManager):
+    def _create_user(self, email, phone, **extra_fields):
+        if not email:
+            raise ValueError("You havent nt provided a valid email")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password, **extra_fields)
