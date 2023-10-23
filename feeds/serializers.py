59d9d8ff1452a0cc4aee4728ed3c2ea1ae9372f4
abednego emonena <emonnenaabednego@gmail.com>
2023-10-23 22:24:39 +0100

    updated the story serializer to include the actual content

--- feeds/serializers.py
+++ feeds/serializers.py
@@ -4,16 +4,32 @@ from django.contrib.contenttypes.models import ContentType
 from django.db.models import Q
 
 
+from rest_framework import serializers
+from .models import Story, StoryText, StoryImage, StoryVideo
+from django.db.models import Q
+
 class StorySerializer(serializers.ModelSerializer):
     content_type_id = serializers.IntegerField(write_only=True)
+    content = serializers.SerializerMethodField()
     reactions_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Story
-        fields = ('id', 'user', 'object_id', 'content_type_id', 'reactions_count')
+        fields = ('id', 'user', 'object_id', 'content_type_id', 'content', 'reactions_count')
 
     def get_reactions_count(self, obj):
         return obj.story_reactions.count()
+
+    def get_content(self, obj):
+        content_object = obj.content_object
+        if isinstance(content_object, StoryText):
+            return content_object.text
+        elif isinstance(content_object, StoryImage):
+            return content_object.image.url
+        elif isinstance(content_object, StoryVideo):
+            return content_object.video.url
+        else:
+            return None
 
     def create(self, validated_data):
         content_type_id = validated_data.pop('content_type_id')
@@ -26,7 +42,7 @@ class StorySerializer(serializers.ModelSerializer):
             3: StoryVideo,
         }
 
-        model_class = CONTENT_TYPE_MAP.get(content_type_id)      
+        model_class = CONTENT_TYPE_MAP.get(content_type_id)
 
         if model_class is None:
             raise serializers.ValidationError("Invalid content type")
@@ -37,6 +53,12 @@ class StorySerializer(serializers.ModelSerializer):
         content_object = model_class.objects.get(id=object_id)
         story = Story.objects.create(user=user, content_object=content_object)
         return story
+
+    def to_representation(self, instance):
+        representation = super(StorySerializer, self).to_representation(instance)
+        representation['content'] = self.get_content(instance)
+        return representation
+
 
 
 class StoryImageSerializer(serializers.ModelSerializer):
