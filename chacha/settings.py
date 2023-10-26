f80ac4e3e441f8b8bed249aab70175f1f36da196
Abednego Emonena <emonenaabednego@gmail.com>
2023-10-26 12:16:48 +0100

    added an envionmental variable to .env

--- chacha/settings.py
+++ chacha/settings.py
@@ -20,7 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_3)u$ziq_b&$ppkzk60i!9j*yssv@1hb@jfp=2^3o=*10j_e#7'
+
+import os
+from decouple import config
+
+# ...
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
