ccdbde28a03018e69bbdb944465502e22b876373
Abednego Emonena <emonenaabednego@gmail.com>
2023-10-28 15:16:28 +0100

    add a field room to the message model

--- chat/models.py
+++ chat/models.py
@@ -7,7 +7,7 @@ from userauth.models import User
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE)
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receivers')
-    room = models.CharField(max_length=100)
+    room = models.CharField(max_length=100, null=True, blank=True)
     message = models.TextField(max_length=1000)
     is_read = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
